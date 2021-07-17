d58ac936e3124f3e58700ae54614dc208ea0940d
KentaMatsu <matsuken102@gmail.com>
2021-07-17 21:01:08 +0000

    start4-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cd36cf1ee389ab74c506b441abb4b4c1e9af75d78e7e718baeaac95a340b5046d7384ebcf287b157fb5531deeed77b6a2c91f92a316d2d7e8212c2fe524a24c2'
